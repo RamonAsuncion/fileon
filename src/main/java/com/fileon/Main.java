1dac569c35cc017ab6fd84753f80a13af540945a
Ramon Asuncion <ramon@b1t.dev>
2024-11-26 20:53:47 -0500

    Add a mainframe.

--- src/main/java/com/fileon/Main.java
+++ src/main/java/com/fileon/Main.java
@@ -1,20 +1,17 @@
 package com.fileon;
 
-// import javax.swing.*;
-// import java.awt.*;
-import java.io.IOException;
-import java.util.Properties;
+import com.fileon.ui.MainFrame;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            ConfigReader reader = new ConfigReader("config.properties");
-            Properties properties = reader.getProperties();
-            String appName = properties.getProperty("app.name");
-            System.out.println("App name: " + appName);
-        } catch (IOException e) {
-            System.err.println("Error reading config file: " + e.getMessage());
-        }
+        MainFrame mainFrame = new MainFrame();
+
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                mainFrame.setVisible(true);
+            }
+        });
     }
 
 }
--- src/main/java/com/fileon/ui/MainFrame.java
+++ src/main/java/com/fileon/ui/MainFrame.java
@@ -1,5 +1,20 @@
 package com.fileon.ui;
 
-public class MainFrame {
+import javax.swing.*;
 
+// try {
+// ConfigReader reader = new ConfigReader("config.properties");
+// Properties properties = reader.getProperties();
+// String appName = properties.getProperty("app.name");
+// System.out.println("App name: " + appName);
+// } catch (IOException e) {
+// System.err.println("Error reading config file: " + e.getMessage());
+// }
+
+public class MainFrame extends JFrame {
+    public MainFrame() {
+        setTitle("App");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(500, 500);
+    }
 }
